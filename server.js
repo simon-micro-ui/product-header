6cca32928de69aa290c289d81efc269b67b9d5df
simoncurran <n0118472@libp45p-39675fl.mis.lmig.com>
2018-06-18 11:39:34 +0100

    Updates to serve the webcomponent via node express server.

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/product-header'));
 
-app.get('/*', function(req,res) {
+app.get('/index.html', function(req,res) {
     console.log('__dirname:'+__dirname);
-res.sendFile(path.join(__dirname+'/dist/product-header/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/product-header/index.html'));
+});
+app.get('/product-header.js', function(req,res) {
+    console.log('__dirname:'+__dirname);
+    res.sendFile(path.join(__dirname+'/dist-npm/product-header/product-header.js'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+console.log("Starting server on port 8081");
+app.listen(process.env.PORT || 8081);
